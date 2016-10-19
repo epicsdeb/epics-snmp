67efa73336047f1e4717e46bd642ed42683891aa
Daron Chabot <chabot@frib.msu.edu>
2016-10-19 17:48:32 -0400

    CLN: pkg'ing cruft

--- debian/epics-snmp.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the epics-snmp package
-#
-0 4	* * *	root	[ -x /usr/bin/epics-snmp_maintenance ] && /usr/bin/epics-snmp_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(epics-snmp):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="epics-snmp" command="/usr/bin/epics-snmp"
